287a21be005273f3a9eae52e77e622a1af9ed0d6
Raymond Xie <raymond.y.xie@gmail.com>
2019-01-17 17:28:55 -0800

    base active search

--- pick-a-client/src/components/ActiveSearch.js
+++ pick-a-client/src/components/ActiveSearch.js
@@ -4,19 +4,27 @@ import { connect } from 'react-redux'
 class ActiveSearch extends React.Component {
     render(){
         const setting = this.props.data.search
-        let render = this.props.data.search ? 
-        (
+        let location;
+        if(setting){
+            if(setting.currentLocation){
+                location = "Current Location"
+            } else {
+                location = setting.location;
+            }
+        }
+        let render = this.props.data.search ? (
             <div className="container-fluid">
-                <p>Term: {setting.term}</p>
-                {/* <p>{location}</p> */}
-                <p>Location: {setting.location}</p>
-                <p>Open Now: {setting.openNow}</p>
-                <p>Price: {setting.price}</p>
-                <p>Radius: {setting.radius}</p>
+                <div className="row">
+
+                    <p className="col-sm-2 offset-md-1">Term: {setting.term}</p>
+                    {/* <p>{location}</p> */}
+                    <p className="col-sm-2">Location: {location}</p>
+                    <p className="col-sm-2">Open Now: {setting.openNow.toString()}</p>
+                    <p className="col-sm-2">Price: {setting.price}</p>
+                    <p className="col-sm-2">Radius: {Math.round(setting.radius / 1609) + " miles"}</p>
+                </div>
             </div>
-        ) 
-        : 
-        (
+        ) : (
             <div></div>
         )
         return (
