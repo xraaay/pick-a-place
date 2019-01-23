5e890e244bf139285f72f472d5e2e90cb9980ebb
Raymond Xie <raymond.y.xie@gmail.com>
2019-01-22 23:13:10 -0800

    working in electron

--- pick-a-client/src/layout/Nav.js
+++ pick-a-client/src/layout/Nav.js
@@ -50,46 +50,63 @@ class Nav extends React.Component {
         const navbar = route === "/" ? null : {zIndex: 0, position: "inherit"}
         const title = route === "/" ? null : {color: '#161616'}
         const buttons = route === "/" ? null : {color: 'rgba(0, 0, 0, 0.5)'}
-        const loggedIn = this.props.user.user ? (
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 1)}} style={buttons}>Log In</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 2)}} style={buttons}>Sign Up</a>
-                </li>
-            </ul>
-        ) : (
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 6)}} style={buttons}>Would You Rather</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 5)}} style={buttons}>Roll The Dice</a>
-                </li>
-                <li className="nav-item">
-                    {/* <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 3)}} style={buttons}>Search</a> */}
-                    <SettingsForm buttons={buttons}/>
-                </li>
-                {/* <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 4)}} style={buttons}>History</a>
-                </li> */}
-                {/* <li className="nav-item">
-                    <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 7)}} style={buttons}>Logout</a>
-                </li> */}
-            </ul>
-        )
+        // const loggedIn = this.props.user.user ? (
+        //     <ul className="navbar-nav ml-auto">
+        //         <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 1)}} style={buttons}>Log In</a>
+        //         </li>
+        //         <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 2)}} style={buttons}>Sign Up</a>
+        //         </li>
+        //     </ul>
+        // ) : (
+        //     <ul className="navbar-nav ml-auto">
+        //         <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 6)}} style={buttons}>Would You Rather</a>
+        //         </li>
+        //         <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 5)}} style={buttons}>Roll The Dice</a>
+        //         </li>
+        //         <li className="nav-item">
+        //             {/* <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 3)}} style={buttons}>Search</a> */}
+        //             <SettingsForm buttons={buttons}/>
+        //         </li>
+        //         {/* <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 4)}} style={buttons}>History</a>
+        //         </li> */}
+        //         {/* <li className="nav-item">
+        //             <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 7)}} style={buttons}>Logout</a>
+        //         </li> */}
+        //     </ul>
+        // )
         return (
             <React.Fragment>
                     <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav" style={navbar}>
                         <div className="container">
                             <a href="#" className="navbar-brand js-scroll-trigger"style={title} onClick={e => {this.navigate(e, 0)}}>Pick a Place</a>
-                            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                            Menu
-                            <i className="fas fa-bars"></i>
+                            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">                            
+                                Menu
+                                <i className="fas fa-bars"></i>
                             </button>
                             <div className="collapse navbar-collapse" id="navbarResponsive">
-                                {loggedIn}
+                                <ul className="navbar-nav ml-auto">
+                                    <li className="nav-item">
+                                        <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 6)}} style={buttons}>Would You Rather</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 5)}} style={buttons}>Roll The Dice</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        {/* <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 3)}} style={buttons}>Search</a> */}
+                                        <SettingsForm buttons={buttons}/>
+                                    </li>
+                                    {/* <li className="nav-item">
+                                        <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 4)}} style={buttons}>History</a>
+                                    </li> */}
+                                    {/* <li className="nav-item">
+                                        <a href="#" className="nav-link js-scroll-trigger" onClick={e => {this.navigate(e, 7)}} style={buttons}>Logout</a>
+                                    </li> */}
+                                </ul>
                             </div>
                         </div>
                     </nav>
