aa1e793a1900aa9f95220382d9b1f31de556767d
Raymond Xie <raymond.y.xie@gmail.com>
2019-01-26 22:02:39 -0800

    build

---
+++ PickAnAPI/PickAnAPI/wwwroot/build/precache-manifest.11182999decbf6e7906d0ead22bd06e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "af9987b8e6379f7be8ab",
+    "url": "/static/js/main.af9987b8.chunk.js"
+  },
+  {
+    "revision": "4e39ea8b5a059de0bd4e",
+    "url": "/static/js/1.4e39ea8b.chunk.js"
+  },
+  {
+    "revision": "af9987b8e6379f7be8ab",
+    "url": "/static/css/main.e4b17dab.chunk.css"
+  },
+  {
+    "revision": "4e39ea8b5a059de0bd4e",
+    "url": "/static/css/1.3ff37302.chunk.css"
+  },
+  {
+    "revision": "cedd52dc705e6d55ad7b11ff3711035c",
+    "url": "/index.html"
+  }
+];
--- PickAnAPI/PickAnAPI/wwwroot/build/service-worker.js
+++ PickAnAPI/PickAnAPI/wwwroot/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.502a8fd24b46ce6e8e31ed7a073ab710.js"
+  "/precache-manifest.11182999decbf6e7906d0ead22bd06e3.js"
 );
 
 workbox.clientsClaim();
